6aa2d4c8b0a1339028921aa7ac9503ab82c48499
Egor-Shalashov <80955040+Egor-Shalashov@users.noreply.github.com>
2022-05-14 00:49:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//console.log("PPPPPPPPP");
-
 const btn = document.querySelector('#btn_add');
 const input = document.querySelector('#input');
 
@@ -28,29 +26,3 @@ btn.addEventListener('click', event => {
         }
     }
 });
-
-// document.querySelector('#btn_add').onclick = function(){
-//     if(document.querySelector('#create_a_task input').value.length == 0){
-//         alert("Please Enter a Task")
-//     }
-//
-//     else{
-//         document.querySelector('#tasks').innerHTML += `
-//             <div class="task">
-//                 <span id="taskname">
-//                     ${document.querySelector('#create_a_task input').value}
-//                 </span>
-//                 <button class="delete">
-//                     <i class="far fa-trash-alt"></i>
-//                 </button>
-//             </div>
-//         `;
-//
-//         var current_tasks = document.querySelectorAll(".delete");
-//         for(var i=0; i<current_tasks.length; i++){
-//             current_tasks[i].onclick = function(){
-//                 this.parentNode.remove();
-//             }
-//         }
-//     }
-//
